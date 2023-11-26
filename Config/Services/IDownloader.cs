753e08096535986cadf990f22d6223c52e54852a
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-11-26 19:58:19 +0100

    d update behaviour

--- Config/Services/IDownloader.cs
+++ Config/Services/IDownloader.cs
@@ -7,6 +7,7 @@ public interface IDownloader : IDisposable
     /// <summary>
     ///     Downloads the configuration from somewhere into the specified path.
     ///     Avoid downloading into previously downloaded paths.
+    ///     If the path already exists, the content is updated to the latest version.
     /// </summary>
     /// <param name="path">Path where the configuration is downloaded into.</param>
     /// <returns><see cref="Empty" /> or an error.</returns>
